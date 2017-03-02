7f9f01b2bf7b54a0bf2cb54696c2bf72df191153
skoyama427 <skoyama@kuhp.kyoto-u.ac.jp>
2017-03-03 01:26:13 +0900

    Ceftools working

--- Ceftools.R
+++ Ceftools.R
@@ -3,45 +3,37 @@ readCEF <- function(INFILE){
   print("extract dataset information...")
 
   INFO <- list()
-  for(i in 1:3){
-    TMP <- read.table(INFILE, skip=i-1, nrow=1)
-    INFO[i] <- paste0(TMP, collapse=" ")
-  }
-
-
-  print("extract cell information...")
-
-  for(i in 4:7){
-    INFO[[i]] <- unlist(read.table(INFILE, skip=i-1, nrow=1))[-1]
-  }
-
+  LEG <- read.table(INFILE, nrow=8, fill=T)[,1]
+  
+  INFO[[1]] <- read.table(INFILE, skip=(which(LEG=="CEF")      -1), nrow=1) %>>% paste0
+  INFO[[2]] <- read.table(INFILE, skip=(which(LEG=="Cell_ID")  -1), nrow=1)[-1]
+  INFO[[3]] <- read.table(INFILE, skip=(which(LEG=="Cell_type")-1), nrow=1)[-1]
+  INFO[[4]] <- read.table(INFILE, skip=(which(LEG=="Timepoint")-1), nrow=1)[-1]
 
   print("making expression matrix...")
 
-  TMP <- read.table(INFILE, skip=8)
+  TMP <- read.table(INFILE, skip=(which(LEG=="Gene")))
   EXPR <- as.matrix(TMP[,-1])
   rownames(EXPR) <- TMP[,1]
-  colnames(EXPR) <- INFO[[4]]
-
+  colnames(EXPR) <- INFO[[2]]
 
   print("make output list for analysis...")
 
   OUT <- list(CEF             = INFO[[1]],
-              Dataset         = INFO[[2]],
-              InfoVersion     = INFO[[3]],
-              Cell_ID         = INFO[[4]],
-              Cell_type       = INFO[[5]],
-              Timepoint       = INFO[[6]],
-              Total_Molecules = INFO[[7]],
+              Cell_ID         = INFO[[2]],
+              Cell_type       = INFO[[3]],
+              Timepoint       = INFO[[4]],
               Readcount       = EXPR)
 
   return(OUT)
 }
 
-INFILE <- "../../data/Human_Embryo_fulldataset.cef"
+INFILE <- "Human_Embryo_fulldataset.cef"
+INFILE <- "Mouse_Embryo_fulldataset.cef"
+INFILE <- "iPSC_fulldataset.cef"
+INFILE <- "ES_fulldataset.cef"
 
 OUT <- readCEF(INFILE)
+OUT$Readcount %>>% hh
+OUT$Readcount %>>% dim
 
-head(OUT[[4]])
-
-OUTd[[4]]
